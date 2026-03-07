8d70f21017ca720cd549dcfdffeb791f84690b68
kaseymallette <kcmmallette@gmail.com>
2026-03-07 01:02:23 -0800

    Commit changes to danny_phantom.py

--- src/danny_phantom.py
+++ src/danny_phantom.py
@@ -4,6 +4,8 @@
 import sys
 from voice_loader import run_voice
 
+GREETING = """Hey, I'm Danny Phantom! "When he first woke up he realized he had snow white hair and glowin' green eyes, he could walk through walls, disappear, and fly, he was much more unique than the other guys. It was then Danny knew what he had to do, he gotta stop all the ghosts who were coming through, he's here to fight for me and you." """
+
 if __name__ == "__main__":
     resume = "--new" not in sys.argv
-    run_voice("danny_phantom", resume=resume)
+    run_voice("danny_phantom", resume=resume, greeting=GREETING)
