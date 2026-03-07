b37709d4a59dd950e432e2f76879a896ae82de0d
kaseymallette <kcmmallette@gmail.com>
2026-03-07 00:59:12 -0800

    Commit changes to casper.py

--- src/casper.py
+++ src/casper.py
@@ -4,6 +4,8 @@
 import sys
 from voice_loader import run_voice
 
+GREETING = "Hey! I'm Casper. Hope I don't scare you. I'm a friendly ghost... Boo!"
+
 if __name__ == "__main__":
     resume = "--new" not in sys.argv
-    run_voice("casper", resume=resume)
+    run_voice("casper", resume=resume, greeting=GREETING)
